68cd09af81325c183f617da1f88239a697947f29
Karel Minarik <karmi@karmi.cz>
2010-10-24 18:17:50 +0200

    Make sure we are running a Bash shell (symlink and check)

--- packages/build-essential.rb
+++ packages/build-essential.rb
@@ -5,4 +5,14 @@ package :build_essential do
   apt 'build-essential' do
     pre :install, 'apt-get update'
   end
+
+  apt 'bash' do
+    # Make sure we are running a Bash shell
+    post :install, 'ln -sf /bin/bash /bin/sh'
+  end
+
+  verify do
+    has_executable '/bin/bash'
+    has_symlink    '/bin/sh', '/bin/bash'
+  end
 end
