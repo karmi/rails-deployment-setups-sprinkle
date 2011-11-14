bd1a31864973cbbd56544687e083e5c03a1fd2ac
Karel Minarik <karmi@karmi.cz>
2011-11-14 17:33:10 +0100

    Upgrade Rubygems to 1.8.11

--- packages/rubygems.rb
+++ packages/rubygems.rb
@@ -1,7 +1,7 @@
 package :rubygems do
   requires :ruby
   description 'Ruby Gems Package Management System'
-  version '1.3.7'
+  version '1.8.11'
 
   source "http://production.cf.rubygems.org/rubygems/rubygems-#{version}.tgz" do
     custom_install 'ruby setup.rb'
