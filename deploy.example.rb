c9310456efb7b41dc627486b9cfb5d45a062b5d4
Karel Minarik <karmi@karmi.cz>
2011-11-15 18:14:57 +0100

    Annotated the deploy.example.rb file

--- deploy.example.rb
+++ deploy.example.rb
@@ -1,14 +1,24 @@
+# Example configuration file for Capistrano Sprinkle deploy strategy
+#
+#
 # -- Application configuration
 role :app,  '<CHANGE THIS TO YOUR SERVER IP>', :primary => true
 
 # --- SSH connection configuration
 #
-# Regular user
 set  :user, '<CHANGE THIS TO YOUR USER>'
 #
-# Root user
-# set  :user, 'root'
-# set  :use_sudo, false
-# set  :run_method, :run
+# --  SSH options
 #
+# -   Port
+# ssh_options[:port] = 22
+#
+# -   Authenticate with key pair, not password
+# ssh_options[:keys] = [ File.dirname('/path/to/your/private/key') ]
+#
+# -   Use sudo
+# set  :use_sudo, false
+#
+# -   Declare intareactive terminal
 default_run_options[:pty] = true
+#
